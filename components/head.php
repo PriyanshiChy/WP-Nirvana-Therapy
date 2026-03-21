2b13ed16c437f671aa1366c81eff98f165c34874
Ahmed Mawia <ahmedmawia.dev@gmail.com>
2026-03-21 21:08:41 +0530

    feat: added webp for better loading

--- components/head.php
+++ components/head.php
@@ -9,8 +9,6 @@
   <meta name="creator" content="Priyanshi Choudhury <priyanshichoudhury.work@gmail.com>" />
   <meta name="author" content="Nirvana Therapy" />
 
-  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon.ico" type="image/x-icon">
-
   <meta property="og:title" content="<?php echo $args["props"]["title"]; ?>" />
   <meta property="og:description" content="<?php echo $args["props"]["description"]; ?>" />
   <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/images/og-image.png" />
@@ -22,10 +20,20 @@
   <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/assets/images/twitter-card.png" />
 
   <!-- Favicon -->
-  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon.ico" type="image/x-icon" />
-  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon-16x16.png" sizes="16x16">
-  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/apple-touch-icon.png">
+  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon.ico" type="image/x-icon" />
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-16x16.png" sizes="16x16">
+  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/favicon/apple-touch-icon.png">
+
+  <!-- Preload images -->
+  <link rel="preload" as="image" href="<?php echo get_template_directory_uri(); ?>/assets/images/bg/hero.webp" />
+  <link rel="preload" as="image" href="<?php echo get_template_directory_uri(); ?>/assets/images/bg/tnc.webp" />
+  <!-- Prefetch images -->
+  <link rel="prefetch" as="image" href="<?php echo get_template_directory_uri(); ?>/assets/images/bg/about.webp" />
+  <link rel="prefetch" as="image" href="<?php echo get_template_directory_uri(); ?>/assets/images/bg/bg-footer.webp" />
+  <link rel="prefetch" as="image" href="<?php echo get_template_directory_uri(); ?>/assets/images/bg/choose-us.webp" />
+  <link rel="prefetch" as="image" href="<?php echo get_template_directory_uri(); ?>/assets/images/bg/founder.webp" />
+  <link rel="prefetch" as="image" href="<?php echo get_template_directory_uri(); ?>/assets/images/bg/services.webp" />
 
   <!-- Theme main stylesheet (style.css in theme root) -->
   <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" />
